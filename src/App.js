bc040d5f12ea029f8a4e90f07b0f84b2cfcce28d
jkirkpatrick24 <jkirkpat16@gmail.com>
2020-08-26 15:44:22 -0700

    absolute-impoty

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { ReactComponent as Icon } from './assets/Icon.svg';
+import { ReactComponent as Icon } from 'assets/Icon.svg';
 
 function App() {
   return (
