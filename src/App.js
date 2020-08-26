a1e13bcb6a037b543e0891d3709eda263053f946
jkirkpatrick24 <jkirkpat16@gmail.com>
2020-08-26 15:38:25 -0700

    .

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { ReactComponent as Icon } from './assets/Icon.svg';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Icon />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
